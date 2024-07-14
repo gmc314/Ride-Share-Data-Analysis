fa962fbf071a226840f315232ea23888336769fa
gmc314 <grant.cheng1@gmail.com>
2024-07-14 13:02:41 -0400

    add sql query for weekday freq

---
+++ freq_of_rides_across_weekdays_and_member_casual.sql
@@ -0,0 +1,5 @@
+-- dataset uploaded to Google BigQuery
+SELECT member_casual, day_of_week, ROUND(COUNT(*) * 100 / SUM(COUNT(*)) OVER(), 2) || '%' AS percentage_of_total
+FROM `oceanic-actor-427116-b6.ride_share.2023july` 
+GROUP BY member_casual, day_of_week 
+ORDER BY  member_casual, day_of_week

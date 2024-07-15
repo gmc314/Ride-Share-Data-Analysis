7088860a1f93ac5fd02c07fb6946316a6944412f
gmc314 <grant.cheng1@gmail.com>
2024-07-15 13:55:07 -0400

    add folder for sql queries

--- sql_queries/ride_distance_stats_over_days_of_week_and_member_casual.sql
+++ sql_queries/ride_distance_stats_over_days_of_week_and_member_casual.sql
@@ -1,3 +1,4 @@
+-- dataset uploaded to Google BigQuery
 SELECT member_casual, day_of_week, AVG(ride_distance_meters) AS avg_ride_distance, MAX(ride_distance_meters) AS max_ride_distance, MIN(ride_distance_meters) AS min_ride_distance
 FROM `oceanic-actor-427116-b6.ride_share.july2023`
 GROUP BY member_casual, day_of_week

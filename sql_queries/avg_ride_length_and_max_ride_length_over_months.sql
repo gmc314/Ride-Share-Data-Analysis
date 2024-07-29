a61e1271a41688f6d070559ddf51256be7d9727b
gmc314 <grant.cheng1@gmail.com>
2024-07-29 17:40:33 -0400

    modify queriees

--- sql_queries/avg_ride_length_and_max_ride_length_over_months.sql
+++ sql_queries/avg_ride_length_and_max_ride_length_over_months.sql
@@ -1,13 +1,10 @@
 -- dataset uploaded to Google BigQuery
-SELECT member_casual,
-        EXTRACT(YEAR FROM started_at) AS yr, 
-        EXTRACT(MONTH FROM started_at) AS mth,
-        AVG(ride_length_seconds) AS avg_ride_time,
-        MAX(ride_length_seconds) AS max_ride_time
-FROM `oceanic-actor-427116-b6.ride_share.fullyear`
-GROUP BY member_casual,
-          yr,
-          mth
-ORDER BY member_casual,
-          yr,
-          mth
+SELECT CONCAT(yr, "-", mth) AS months, avg_ride_seconds
+FROM  
+  (SELECT EXTRACT(YEAR FROM started_at) AS yr, 
+       EXTRACT(MONTH FROM started_at) AS mth, 
+       ROUND(AVG(ride_length_seconds), 4) AS avg_ride_seconds
+  FROM `oceanic-actor-427116-b6.ride_share.fullyear`
+  WHERE member_casual = "member"
+  GROUP BY yr, mth
+  ORDER BY yr, mth)
--- sql_queries/freq_of_bike_types_across_members_and_casuals.sql
+++ sql_queries/freq_of_bike_types_across_members_and_casuals.sql
@@ -1,5 +1,5 @@
 -- dataset uploaded to Google BigQuery
 SELECT member_casual, rideable_type, ROUND(COUNT(*) / SUM(COUNT(*)) OVER(), 2) * 100 || '%' AS percent_of_total
-FROM `oceanic-actor-427116-b6.ride_share.2023july` 
+FROM `oceanic-actor-427116-b6.ride_share.fullyear` 
 GROUP BY member_casual, rideable_type
 ORDER BY member_casual DESC
